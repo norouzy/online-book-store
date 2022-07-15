b1bd0a9ad62e00b3bbb68a94db158bf63ddd7767
norouzy <mohamadnewday@gmail.com>
2022-07-15 16:03:11 +0430

    removed some comment

--- scripts/panel.py
+++ scripts/panel.py
@@ -1287,13 +1287,3 @@ class Ui_MainWindow(object):
             self.bookCatBoxes[bx].setText(_translate("MainWindow", objName))
             if self.is_admin:
                 self.categoryBoxes[bx].setText(_translate("MainWindow", objName))
-
-
-# if __name__ == "__main__":
-#     import sys
-#     app = QtWidgets.QApplication(sys.argv)
-#     MainWindow = QtWidgets.QMainWindow()
-#     ui = Ui_MainWindow()
-#     ui.setupUi(MainWindow)
-#     MainWindow.show()  
-#     sys.exit(app.exec_())
